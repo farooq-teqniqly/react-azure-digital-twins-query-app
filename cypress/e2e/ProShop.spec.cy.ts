e00aeeb1cb9e6a30a7064002cf8562e4b6466090
farooq-teqniqly <farooq@teqniqly.com>
2024-01-31 11:18:47 -0800

    Query page

--- cypress/e2e/ProShop.spec.cy.ts
+++ cypress/e2e/ProShop.spec.cy.ts
@@ -7,6 +7,19 @@ describe("ProShop", () => {
     cy.visit(localAppUrl);
 
     const h1 = getBySelector("h1");
-    assertTextExact(h1, "Pro Shop");
+    assertTextExact(h1, "Azure Digital Twins Query App");
+  });
+
+  it("executes the query typed by the user", () => {
+    cy.visit(localAppUrl);
+
+    const queryInput = getBySelector("#query-input");
+    queryInput.type("SELECT * FROM digitaltwins");
+
+    const executeButton = getBySelector("#query-execute-button");
+    executeButton.click();
+
+    const results = getBySelector("#query-results");
+    assertTextExact(results, "[]");
   });
 });
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,51 @@
-import React from "react";
+import Stack from "react-bootstrap/Stack";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function HomePage() {
-  return <h1>Pro Shop</h1>;
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col>
+            <h1 className="mt-3">Azure Digital Twins Query App</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Stack direction="horizontal" gap={3} className="py-3">
+              <Form.Control
+                id="query-input"
+                as="textarea"
+                className="me-auto"
+                placeholder="Type your query here..."
+              ></Form.Control>
+              <Button id="query-execute-button" variant="primary">
+                Execute
+              </Button>
+            </Stack>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h2 className="mt-3">Results</h2>
+            <div>
+              <Form.Control
+                id="query-results"
+                as="textarea"
+                className="me-auto"
+                placeholder="Type your query here..."
+                value="[]"
+              ></Form.Control>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
 
 export default HomePage;
